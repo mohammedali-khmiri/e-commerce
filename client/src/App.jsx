8906e2bf404be6061c8f87aee47c8c46c55f58a3
medAli <khemirimohamedali7@gmail.com>
2022-05-15 14:41:11 +0100

    add client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+
+const App = () => {
+	const user = true
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/products/:category" element={<ProductList />} />
+				<Route path="/product/:id" element={<Product/>} />
+				<Route path="/cart" element={<Cart/>} />
+				<Route path="/login" element={user ? <Navigate  to="/"/> : <Login/>}/>
+				<Route path="/register" element={user ? <Navigate  to="/"/> : <Register/>}/>
+			</Routes>
+		</BrowserRouter>
+	);
+};
+export default App;
